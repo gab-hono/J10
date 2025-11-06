000c3f18f07e19c3ced868cb48364d40c6bb897d
Gabriel H. <gabrielhono.o@gmail.com>
2025-11-06 15:40:13 +0100

    fix structure du code

--- 01_pokedex/scriptpourhtml.js
+++ 01_pokedex/scriptpourhtml.js
@@ -6,27 +6,38 @@ fetch("pokedex.json")
     pokedex = data;
     console.log("✅ Pokedex chargé !");
 
-    const searchButton = document.getElementById("search-button");
-    const inputField = document.getElementById("pokemon-input");
+const searchButton = document.getElementById("search-button");
+const inputField = document.getElementById("pokemon-input");
 
-    searchButton.addEventListener("click", () => {
-      displayPokemonInfo(inputField.value.trim());
-    });
+searchButton.addEventListener("click", () => {
+  displayPokemonInfo(inputField.value.trim());
+});
 
-    inputField.addEventListener("keyup", (event) => {
-      if (event.key === "Enter") {
-        displayPokemonInfo(inputField.value.trim());
-      }
-    });
-  })
-  .catch(err => console.error("❌ Erreur:", err));
+inputField.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    displayPokemonInfo(inputField.value.trim());
+  }
+});
+})
+  
+.catch(err => console.error("❌ Erreur:", err));
 
 function searchPokemon(name) {
   const searchedPokemon = pokedex.pokemon.find(p => p.name.toLowerCase() === name.toLowerCase());
   if (!searchedPokemon) return null;
 
-  const nextEvol = searchedPokemon.next_evolution ? searchedPokemon.next_evolution.map(e => e.name).join(" → ") : "Aucune";
-  const prevEvol = searchedPokemon.prev_evolution ? searchedPokemon.prev_evolution.map(e => e.name).join(" → ") : "Aucune";
+  let nextEvol;
+  if (searchedPokemon.next_evolution) {
+    nextEvol = searchedPokemon.next_evolution.map(e => e.name).join(" → ")
+  } else {
+    nextEvol = "Aucune"
+  } 
+  let prevEvol;
+    if (searchedPokemon.prev_evolution) {
+    nextEvol = searchedPokemon.prev_evolution_evolution.map(e => e.name).join(" → ")
+  } else {
+    nextEvol = "Aucune"
+  } 
 
   return {
     name: searchedPokemon.name,
